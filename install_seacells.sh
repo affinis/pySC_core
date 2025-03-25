443c8e28848d41c573e26a13b2722bf0020fb04c
affinis <affinis@sjtu.edu.cn>
2025-03-25 16:20:24 +0800

    modified seacells install script

--- install_seacells.sh
+++ install_seacells.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#this pipeline refer to https://github.com/dpeerlab/SEACells
+#Lin Lyu 2025.3.25
+
 set -e
 
 #create environment
@@ -9,3 +12,9 @@ mamba activate seacells
 #set mirror for pip to accelerate downloadig, conda/mamba do not include seacells package
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 pip install seacells
+
+#install jupyterlab for learning of 'seacells'
+mamba install jupyterlab
+
+#download notebook file for learning of 'seacells'
+wget https://github.com/dpeerlab/SEACells/blob/main/notebooks/SEACell_computation.ipynb
